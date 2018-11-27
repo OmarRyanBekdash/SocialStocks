63fdd5ebe3a2fc5baf1c9b6227fcd65f21388865
WilliamRockwellEvans <45379622+WilliamRockwellEvans@users.noreply.github.com>
2018-11-27 08:18:36 -0500

    Create GIDSignInButtonView.swift

---
+++ SocialStocksApp/GIDSignInButtonView.swift
@@ -0,0 +1,30 @@
+//
+//  GIDSignInButtonView.swift
+//  InvestmentHelper
+//
+//  Created by William Rockwell Evans on 11/25/18.
+//  Copyright © 2018 William Rockwell Evans. All rights reserved.
+//
+
+import UIKit
+
+class GIDSignInButtonView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
