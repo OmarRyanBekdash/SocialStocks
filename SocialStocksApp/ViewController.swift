8b1e60aeccf7b5bc9ca607bdbaaf2a6207341a73
WilliamRockwellEvans <45379622+WilliamRockwellEvans@users.noreply.github.com>
2018-11-27 00:29:01 -0500

    Update ViewController.swift

--- SocialStocksApp/ViewController.swift
+++ SocialStocksApp/ViewController.swift
@@ -2,18 +2,160 @@
 //  ViewController.swift
 //  SocialStocksApp
 //
-//  Created by Omar Beckdash on 11/26/18.
+//  Created by Omar Beckdash and William Evans on 11/26/18.
 //  Copyright © 2018 Omar Bekdash. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+//    var email: UITextField!
+//    var username: UITextField!
+//    var password: UITextField!
+//
+//    var emailLabel: UILabel!
+//    var usernameLabel: UILabel!
+//    var passwordLabel: UILabel!
+//
+//    var submitButton: UIButton!
+//
+//    var logInButton: UIButton!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//        title = "Make an Account"
+//        view.backgroundColor = .white
+//
+//        email = UITextField()
+//        email.translatesAutoresizingMaskIntoConstraints = false
+//        email.text = "Email"
+//        email.borderStyle = .roundedRect
+//        email.backgroundColor = .white
+//        email.textAlignment = .center
+//        view.addSubview(email)
+//
+//        username = UITextField()
+//        username.translatesAutoresizingMaskIntoConstraints = false
+//        username.text = "Username"
+//        username.borderStyle = .roundedRect
+//        username.backgroundColor = .white
+//        username.textAlignment = .center
+//        view.addSubview(username)
+//
+//        password = UITextField()
+//        password.translatesAutoresizingMaskIntoConstraints = false
+//        password.text = "Password"
+//        password.borderStyle = .roundedRect
+//        password.backgroundColor = .white
+//        password.textAlignment = .center
+//        view.addSubview(password)
+//
+//        emailLabel = UILabel()
+//        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+//        emailLabel.text = "Email: "
+//        emailLabel.textColor = .black
+//        emailLabel.textAlignment = .right
+//        emailLabel.font = .systemFont(ofSize: 25, weight: .regular)
+//        view.addSubview(emailLabel)
+//
+//        usernameLabel = UILabel()
+//        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+//        usernameLabel.text = "Username: "
+//        usernameLabel.textColor = .black
+//        usernameLabel.textAlignment = .right
+//        usernameLabel.font = .systemFont(ofSize: 25, weight: .regular)
+//        view.addSubview(usernameLabel)
+//
+//        passwordLabel = UILabel()
+//        passwordLabel.translatesAutoresizingMaskIntoConstraints = false
+//        passwordLabel.text = "Password: "
+//        passwordLabel.textColor = .black
+//        passwordLabel.textAlignment = .right
+//        passwordLabel.font = .systemFont(ofSize: 25, weight: .regular)
+//        view.addSubview(passwordLabel)
+//
+//        submitButton = UIButton()
+//        submitButton.translatesAutoresizingMaskIntoConstraints = false
+//        submitButton.setTitle("Submit", for: .normal)
+//        submitButton.backgroundColor = .black
+//        submitButton.setTitleColor(.white, for: .normal)
+////        submitButton.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+//        view.addSubview(submitButton)
+//
+//        logInButton = UIButton()
+//        logInButton.translatesAutoresizingMaskIntoConstraints = false
+//        logInButton.setTitle("Already Have an Account: Log In", for: .normal)
+//        logInButton.backgroundColor = .white
+//        logInButton.setTitleColor(.black, for: .normal)
+//        logInButton.addTarget(self, action: #selector(pushSignInNavController), for: .touchUpInside)
+//        view.addSubview(logInButton)
+//
+//        setUpConstraints()
+//
+//    }
+//
+//    func setUpConstraints() {
+//        NSLayoutConstraint.activate([
+//            email.leadingAnchor.constraint(equalTo: view.centerXAnchor),
+//            email.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+//            email.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -80),
+//            email.heightAnchor.constraint(equalToConstant: 25)
+//            ])
+//
+//        NSLayoutConstraint.activate([
+//            username.leadingAnchor.constraint(equalTo: view.centerXAnchor),
+//            username.trailingAnchor.constraint(equalTo: email.trailingAnchor),
+//            username.topAnchor.constraint(equalTo: email.bottomAnchor, constant: 35),
+//            username.heightAnchor.constraint(equalToConstant: 25)
+//            ])
+//
+//        NSLayoutConstraint.activate([
+//            password.leadingAnchor.constraint(equalTo: view.centerXAnchor),
+//            password.trailingAnchor.constraint(equalTo: username.trailingAnchor),
+//            password.topAnchor.constraint(equalTo: username.bottomAnchor, constant: 35),
+//            password.heightAnchor.constraint(equalToConstant: 25)
+//            ])
+//
+//        NSLayoutConstraint.activate([
+//            emailLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+//            emailLabel.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -10),
+//            emailLabel.topAnchor.constraint(equalTo: email.topAnchor),
+//            emailLabel.bottomAnchor.constraint(equalTo: email.bottomAnchor)
+//            ])
+//
+//        NSLayoutConstraint.activate([
+//            usernameLabel.leadingAnchor.constraint(equalTo: emailLabel.leadingAnchor),
+//            usernameLabel.trailingAnchor.constraint(equalTo: emailLabel.trailingAnchor),
+//            usernameLabel.topAnchor.constraint(equalTo: username.topAnchor),
+//            usernameLabel.bottomAnchor.constraint(equalTo: username.bottomAnchor)
+//            ])
+//
+//        NSLayoutConstraint.activate([
+//            passwordLabel.leadingAnchor.constraint(equalTo: emailLabel.leadingAnchor),
+//            passwordLabel.trailingAnchor.constraint(equalTo: emailLabel.trailingAnchor),
+//            passwordLabel.topAnchor.constraint(equalTo: password.topAnchor),
+//            passwordLabel.bottomAnchor.constraint(equalTo: password.bottomAnchor)
+//            ])
+//
+//        NSLayoutConstraint.activate([
+//            submitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            submitButton.topAnchor.constraint(equalTo: password.bottomAnchor, constant: 35),
+//            submitButton.heightAnchor.constraint(equalToConstant: 20)
+//            ])
+//
+//        NSLayoutConstraint.activate([
+//            logInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            logInButton.topAnchor.constraint(equalTo: submitButton.bottomAnchor, constant: 50),
+//            logInButton.heightAnchor.constraint(equalToConstant: 10)
+//            ])
+//    }
+//
+//    @objc func pushSignInNavController() {
+//        let navViewController = SignInNavViewController()
+//        navigationController?.pushViewController(navViewController, animated: true)
+//    }
 
 
 }
