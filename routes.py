dc4682e531a62824ecaf46ebc9342e5d0cf1cc44
WilliamRockwellEvans <45379622+WilliamRockwellEvans@users.noreply.github.com>
2018-11-28 11:11:01 -0500

    Fixed the post_investment function and made create user function

--- routes.py
+++ routes.py
@@ -2,6 +2,8 @@ import json
 from db import db, Users, friends, Investments, Comments
 from flask import Flask, request
 
+
+
 db_filename = "SocialStocksDB"
 app = Flask(__name__)
 
@@ -13,24 +15,42 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+@app.route('/api/user/', methods=['POST'])
+def create_user():
+    body = json.loads(request.data)
+
+    user = Users(
+        email=body.get('email'),
+        username=body.get('username'),
+        password=body.get('password')
+    )
+    db.session.add(user)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': user.serialize()}), 201
+
 @app.route('/api/investments/')
 def get_investments():
     investments = Investments.query.all()
     res = {'success': True, 'data': [investment.serialize() for investment in investments]}
     return json.dumps(res)
 
-@app.route('/api/investments/', methods=['POST'])
-def post_investment():
-    body = json.loads(request.data)
-    investment = Investments(
+@app.route('/api/investments/<int:user_id>/', methods=['POST'])
+def post_investment(user_id):
+    user = Users.query.filter_by(id=user_id).first()
+    if user is not None:
+        body = json.loads(request.data)
+        investment = Investments(
         company = body.get('company'),
         amount = body.get('amount'),
         price = body.get('price'),
-        text = body.get('text')
-    )
-    db.session.add(investment)
-    db.session.commit()
-    return json.dumps({'success': True, 'data': investment.serialize()}), 201
+        text = body.get('text'),
+        user_id=user.id
+        )
+        user.investments.append(investment)
+        db.session.add(investment)
+        db.session.commit()
+        return json.dumps({'success': True, 'data': investment.serialize()}), 201
+    return json.dumps({'success': False, 'error': 'User not found!'}), 404
 
 @app.route('/api/investments/<int:user_id>/')
 def get_user_investments(user_id):
