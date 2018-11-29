7de07a2924909411b68223ad72f5d3298831875e
Maxim Baduk <mbaduk3@gmail.com>
2018-11-28 21:12:22 -0500

    Stock demo - working

--- stock_api.py
+++ stock_api.py
@@ -1,1 +1,14 @@
+"""
+Maxim Baduk
+AlphaVantage api key: 4QJ82DOI81UB69RQ
+"""
+
 import json
+import requests
+
+# An instance sends requests to AlphaVantage to get stock data
+class StockGetter():
+
+    def demo(self):
+        data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo')
+        return json.dumps(data.json())
